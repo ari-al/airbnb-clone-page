77a9770eee934329c90f8376f4f063a56df486cb
arial <billie.ahhyunlish@gmail.com>
2021-08-10 19:48:52 +0900

    고정 헤더 만들기(클래스 네임 교체를 Hook으로 할지 고려 중)

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import '../src/Styles/style.css'
 import Header from './Components/Header';
 import TopNotice from './Components/TopNotice';
 import Main from './Components/Main';
-function App() {
+import React, {useState, useEffect} from 'react';
+
+
+function App(status) {
+  const [fixed, setFixed] = useState("false");
+console.log(fixed);
+  function FixedHeader(){
+    if(window.scrollY>0){
+      setFixed("true");
+    }else{
+      setFixed("false");
+    }
+    return 0;
+  }
+  
+  window.onscroll = function(e) {
+    e.preventDefault();
+    console.log(window.scrollY);
+    FixedHeader();
+    return 0;
+  };
+
   return (
     <div className="App">
       <div style={{position: "relative", zIndex: "1"}}>
         <TopNotice></TopNotice>
-        <Header></Header>
+        <Header fixed={ status.fixed }></Header>
       </div>
       <div style={{ minHeight: "1080px", position: "absolute", top: "0px", width: "100%"}}>
         <Main></Main>
       </div>
     </div>
   );
+  
 }
 
 export default App;
